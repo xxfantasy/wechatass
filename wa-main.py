6f656977ffaf845a96c122c7bb3258962d8d9bac
talen.xie <talenxie@talens-MacBook-Pro.local>
2019-01-23 18:43:16 +0800

    intitial modal

--- wa-main.py
+++ wa-main.py
@@ -1,4 +1,10 @@
 # WA(wechatassistant)
 import itchat
 itchat.auto_login()
-itchat.send('Hello, xxf', toUserName='filehelper')
+# itchat.send('Hello, xxf', toUserName='filehelper')
+
+# 收消息（多线程？）
+
+# 调用SpringFestivaltext
+
+# 发消息（多线程？）
